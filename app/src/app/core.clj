1b50773830858a03a61f6a90c0aba0d8496a2e59
Mouna Cheikhna <mouna.cheikhna@blablacar.com>
2016-05-21 00:52:24 -0700

    Add sol to pb #10

--- app/src/app/core.clj
+++ app/src/app/core.clj
@@ -1,6 +1,15 @@
 (ns app.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; problem 09 
+(defn my-pack [lst]
+  (partition-by identity lst))
+
+;; problem 10
+;; [(n, v)] -> n: nber of occurrences , v value 
+(defn encode [coll]
+  (->> (my-pack coll)
+       (map #(list (count %)(first %)))))
+
+(defn encode2 [coll]
+  (map #(list (count %)(first %)) (my-pack coll)))
+
