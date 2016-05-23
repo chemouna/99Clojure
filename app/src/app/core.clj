0782413c039e8ff23a0b075d4bcec5502da1cd4f
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-05-23 08:01:37 -0700

    Start solution to pb 11

--- app/src/app/core.clj
+++ app/src/app/core.clj
@@ -13,3 +13,11 @@
 (defn encode2 [coll]
   (map #(list (count %)(first %)) (my-pack coll)))
 
+;; problem 11
+(defn encode-modified [coll]
+  (map #( if(= (count %) > 1) (list (count %)(first %)) (identity %))  (my-pack coll)))
+
+;; TODO: fix pb here of calculating count twice
+
+(encode-modified '(a a a a b c c a a d e e e e))
+;; TODO: solve p11 also with core.match
--- app/test/app/core_test.clj
+++ app/test/app/core_test.clj
@@ -2,6 +2,9 @@
   (:require [clojure.test :refer :all]
             [app.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-encode
+  (testing "succeed for a list."
+    (is (= '(a a a a b) '((4, 'a'), (1, 'b')))) ))
+
+
+;; (encode2 '(a a a a b c c a a d e e e e))
