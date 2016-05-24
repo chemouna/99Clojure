f2103329fb0c493c76eb0001a29cf6d34c867303
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-05-23 19:54:30 -0700

    solution to pb 11.

--- app/src/app/core.clj
+++ app/src/app/core.clj
@@ -14,8 +14,24 @@
   (map #(list (count %)(first %)) (my-pack coll)))
 
 ;; problem 11
+;; (defn encode-modified [coll]
+;;   (map #( if (= (count %) > 1) (list (count %)(first %)) (identity %))  (my-pack coll)))
+
+;; (filter ((count %) > 2) (my-pack '(a a a a b c c a a d e e e e)))
+
+;; (->> '((a a a a) (b) (c c) (d) (e e e e))
+;;      (filter #(> 1 (count %)))
+;;      (map second)
+;;      (map first))
+
+
+
+;; solution 11
 (defn encode-modified [coll]
-  (map #( if(= (count %) > 1) (list (count %)(first %)) (identity %))  (my-pack coll)))
+  (let [lst (map #(list (count %)(first %)) (my-pack coll)) ]
+    (map #(if (= (first %) 1)
+            (second %)
+            %) lst)))
 
 ;; TODO: fix pb here of calculating count twice
 
--- app/test/app/core_test.clj
+++ app/test/app/core_test.clj
@@ -2,9 +2,13 @@
   (:require [clojure.test :refer :all]
             [app.core :refer :all]))
 
-(deftest test-encode
-  (testing "succeed for a list."
-    (is (= '(a a a a b) '((4, 'a'), (1, 'b')))) ))
+;; (deftest test-encode
+;;   (testing "succeed for a list."
+;;     (is (= '(a a a a b) '((4, 'a'), (1, 'b')))) ))
 
 
-;; (encode2 '(a a a a b c c a a d e e e e))
+(deftest test-encode-modified
+  (let [lst '(a a a b c c d e e e)
+        ecd '((3 a) b (2 c) d (3 e))]
+    (is (= (encode-modified lst) ecd))))
+
